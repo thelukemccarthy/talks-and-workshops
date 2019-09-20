c1668ebe2ab2d213d2ade10eae4e7ed5951c8149
Luke McCarthy <lmccarth@thoughtworks.com>
2019-09-20 23:09:45 +1000

    [BBB] Add some sample data for the website to render

---
+++ blockchain-devloper-into/bombproof-blockchain-blog/website/src/data/blog-posts.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: "Blog Post",
+    published: "19-Sep-2019",
+    author: "Luke McCarthy",
+    content: "This is a blog post that will live on the blockchain",
+    comments: [{
+      comment: "Comment 01",
+      author: "Comment Author 01",
+    }],
+  },
+  {
+    title: "Blog Post 2",
+    published: "19-Sep-2019",
+    author: "Luke McCarthy",
+    content: "This is another blog post ",
+    comments: [{
+      comment: "Comment 02",
+      author: "Comment Author 02",
+    }],
+  },
+];
