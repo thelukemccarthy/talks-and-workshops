5fa209eead7eefa8d7203833514a1cfca3f892fc
Luke McCarthy <lmccarth@thoughtworks.com>
2019-10-04 08:00:39 +1000

    [BBB] Add basic blog post to the website, reading data in from a js file

--- blockchain-devloper-into/bombproof-blockchain-blog/website/src/App.js
+++ blockchain-devloper-into/bombproof-blockchain-blog/website/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import blogPosts from './data/blog-posts';
+
+const getBlogPosts = () => (
+  blogPosts.map(item => (
+    <div class="blog-post">
+      <h1 class="blog-post-title">{item.title}</h1>
+      <div class="blog-post-publish-date">Publish: {item.published}</div>
+      <div class="blog-post-content"><p>{item.content}</p></div>
+      <div class="blog-post-author">Written by {item.author}</div>
+    </div>
+  ))
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header centerAlign">
+        Bulletproof Blockchain Blog
       </header>
+      <body>
+      {
+        getBlogPosts()}
+      </body>
     </div>
   );
 }
